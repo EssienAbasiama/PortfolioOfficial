a42fbac00b57898a2327728fbf3fcb50b43b2587
EssienAbasiama <80904573+EssienAbasiama@users.noreply.github.com>
2024-02-23 18:53:15 +0100

    Name Change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Essien</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden' />
@@ -24,7 +24,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
+      <div style="margin-top: 50px;"></div>
       <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
