eb7d7b761d266233a59688a8efaac3ea11cf3784
EssienAbasiama <80904573+EssienAbasiama@users.noreply.github.com>
2024-02-23 19:43:18 +0100

    LogoBorderUpdate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' style={{borderRadius:"30px"}}/>
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             KingX &nbsp;
             <span className='sm:block hidden'> | Essien Abasiama</span>
