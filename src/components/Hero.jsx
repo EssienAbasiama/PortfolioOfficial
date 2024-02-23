c6ad3af7d5b12339849078ec98bb7ae622f9dd5f
EssienAbasiama <80904573+EssienAbasiama@users.noreply.github.com>
2024-02-23 19:01:29 +0100

    Minified React Error Fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <div style="margin-top: 50px;"></div>
+      <div style={{ marginTop: '50px' }}></div>;
       <ComputersCanvas />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
