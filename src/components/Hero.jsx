075e4c2d73f1fb6f4b9fdb5be41a4d7ab31f0cf8
EssienAbasiama <80904573+EssienAbasiama@users.noreply.github.com>
2024-02-23 23:57:31 +0100

    Resume to Hello

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -51,7 +51,7 @@ const Hero = () => {
                 download="Resume"
                 class=" "
               >
-                Resume
+                Hello 
               </a>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
