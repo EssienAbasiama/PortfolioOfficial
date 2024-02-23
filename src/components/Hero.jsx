c33f4ab67035266c04bc5ce26f44ae00a2af0b07
EssienAbasiama <80904573+EssienAbasiama@users.noreply.github.com>
2024-02-23 19:27:30 +0100

    UpdateX

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,10 +34,16 @@ const Hero = () => {
             I develop stuning visuals, web <br className="sm:block hidden" />
             applications and mobile applications
           </p>
+          <p class="text-base lg:text-lg pt-3 pb-4">
+            Welcome to my digital world! I specialize in crafting captivating
+            web applications and seamless user interfaces. With creativity and
+            precision, I bring ideas to life and empower businesses to thrive in
+            the ever-evolving digital landscape.
+          </p>
         </div>
       </div>
       <div style={{ marginTop: "50px" }}></div>;
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
