3ca0b06f41013d147606233e8b222e8e90c93da9
EssienAbasiama <80904573+EssienAbasiama@users.noreply.github.com>
2024-02-27 02:11:59 +0100

    new Change

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -70,7 +70,7 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-      <motion.div  variants={fadeIn("", "spring", index * 0.5, 0.75)}>
+      <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
